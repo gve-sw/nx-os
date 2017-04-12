edf76dc13cbf86824fbcc915d395990bf6cb7d9d
Parul Sharma <paruls@cisco.com>
2017-04-12 14:26:27 +0530

    Initial commit for getInterfaces

---
+++ step 1/getInterfaces.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import requests
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--ip',help='Switch IP address', required=True)
+parser.add_argument('-p','--password',help='Switch Username', required=True)
+parser.add_argument('-u','--user',help='Switch user', required=True)
+args = parser.parse_args()
+
+
+"""
+Modify these please
+"""
+url='http://' +args.ip+ '/ins'
+
+switchuser=args.user
+switchpassword=args.password
+
+myheaders={'content-type':'application/json'}
+payload={
+  "ins_api": {
+    "version": "1.0",
+    "type": "cli_show",
+    "chunk": "0",
+    "sid": "1",
+    "input": "show int brief ",
+    "output_format": "json"
+  }
+}
+
+try:
+   response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+   print json.dumps(response['ins_api']['outputs']['output']['body']['TABLE_interface']['ROW_interface'], indent=2, sort_keys=False)
+except requests.exceptions.Timeout:
+    # Maybe set up for a retry, or continue in a retry loop
+    print "timed out connecting to device"
+except requests.exceptions.RequestException as e:
+    # catastrophic error. bail.
+    print e
+    sys.exit(1)
+
+
+
